39c5f04669955c19c73fd3a0380345559c863b64
Wirwing <irving.cf@gmail.com>
2014-03-29 05:00:30 -0600

    Play it again, Sam!

--- appchallenge/conf/evolutions/default/3.sql
+++ appchallenge/conf/evolutions/default/3.sql
@@ -1,41 +1,41 @@
 # --- !Ups
 
-insert into oferta values(7, 1, '2014-01-01', 1, "12:00", "13:20");
-insert into oferta values(7, 1, '2014-01-01', 3, "12:00", "13:20");
-insert into oferta values(7, 1, '2014-01-01', 5, "12:00", "13:20");
-insert into oferta values(7, 2, '2014-01-01', 2, "12:00", "13:20");
-insert into oferta values(7, 2, '2014-01-01', 3, "12:00", "13:20");
-insert into oferta values(7, 2, '2014-01-01', 4, "12:00", "13:20");
+insert into oferta values(7, 1, '2014-01-01', 1, '12:00', '13:20');
+insert into oferta values(7, 1, '2014-01-01', 3, '12:00', '13:20');
+insert into oferta values(7, 1, '2014-01-01', 5, '12:00', '13:20');
+insert into oferta values(7, 2, '2014-01-01', 2, '12:00', '13:20');
+insert into oferta values(7, 2, '2014-01-01', 3, '12:00', '13:20');
+insert into oferta values(7, 2, '2014-01-01', 4, '12:00', '13:20');
 
 
-insert into oferta values(10, 1, '2014-01-01', 1, "15:00", "16:20");
-insert into oferta values(10, 1, '2014-01-01', 3, "15:00", "16:20");
-insert into oferta values(10, 1, '2014-01-01', 5, "15:00", "16:20");
-insert into oferta values(10, 2, '2014-01-01', 1, "16:30", "17:50");
-insert into oferta values(10, 2, '2014-01-01', 3, "16:30", "17:50");
-insert into oferta values(10, 2, '2014-01-01', 5, "16:30", "17:50");
+insert into oferta values(10, 1, '2014-01-01', 1, '15:00', '16:20');
+insert into oferta values(10, 1, '2014-01-01', 3, '15:00', '16:20');
+insert into oferta values(10, 1, '2014-01-01', 5, '15:00', '16:20');
+insert into oferta values(10, 2, '2014-01-01', 1, '16:30', '17:50');
+insert into oferta values(10, 2, '2014-01-01', 3, '16:30', '17:50');
+insert into oferta values(10, 2, '2014-01-01', 5, '16:30', '17:50');
 
-insert into oferta values(11, 1, '2014-01-01', 2, "12:00", "13:20");
-insert into oferta values(11, 1, '2014-01-01', 3, "12:00", "13:20");
-insert into oferta values(11, 1, '2014-01-01', 4, "12:00", "13:20");
-insert into oferta values(11, 2, '2014-01-01', 1, "12:00", "13:20");
-insert into oferta values(11, 2, '2014-01-01', 3, "12:00", "13:20");
-insert into oferta values(11, 2, '2014-01-01', 5, "12:00", "13:20");
-insert into oferta values(11, 3, '2014-01-01', 2, "15:00", "16:20");
-insert into oferta values(11, 3, '2014-01-01', 3, "15:00", "16:20");
-insert into oferta values(11, 3, '2014-01-01', 5, "15:00", "16:20");
+insert into oferta values(11, 1, '2014-01-01', 2, '12:00', '13:20');
+insert into oferta values(11, 1, '2014-01-01', 3, '12:00', '13:20');
+insert into oferta values(11, 1, '2014-01-01', 4, '12:00', '13:20');
+insert into oferta values(11, 2, '2014-01-01', 1, '12:00', '13:20');
+insert into oferta values(11, 2, '2014-01-01', 3, '12:00', '13:20');
+insert into oferta values(11, 2, '2014-01-01', 5, '12:00', '13:20');
+insert into oferta values(11, 3, '2014-01-01', 2, '15:00', '16:20');
+insert into oferta values(11, 3, '2014-01-01', 3, '15:00', '16:20');
+insert into oferta values(11, 3, '2014-01-01', 5, '15:00', '16:20');
 
-insert into oferta values(13, 1, '2014-01-01', 1, "15:00", "16:20");
-insert into oferta values(13, 1, '2014-01-01', 3, "16:30", "17:50");
-insert into oferta values(13, 1, '2014-01-01', 5, "15:00", "16:20");
+insert into oferta values(13, 1, '2014-01-01', 1, '15:00', '16:20');
+insert into oferta values(13, 1, '2014-01-01', 3, '16:30', '17:50');
+insert into oferta values(13, 1, '2014-01-01', 5, '15:00', '16:20');
 
-insert into oferta values(13, 2, '2014-01-01', 1, "15:00", "16:20");
-insert into oferta values(13, 2, '2014-01-01', 3, "15:00", "16:20");
-insert into oferta values(13, 2, '2014-01-01', 5, "15:00", "16:20");
+insert into oferta values(13, 2, '2014-01-01', 1, '15:00', '16:20');
+insert into oferta values(13, 2, '2014-01-01', 3, '15:00', '16:20');
+insert into oferta values(13, 2, '2014-01-01', 5, '15:00', '16:20');
 
-insert into oferta values(13, 3, '2014-01-01', 2, "12:00", "13:20");
-insert into oferta values(13, 3, '2014-01-01', 3, "12:00", "13:20");
-insert into oferta values(13, 3, '2014-01-01', 5, "12:00", "13:20");
+insert into oferta values(13, 3, '2014-01-01', 2, '12:00', '13:20');
+insert into oferta values(13, 3, '2014-01-01', 3, '12:00', '13:20');
+insert into oferta values(13, 3, '2014-01-01', 5, '12:00', '13:20');
 
 
 # --- !Downs
