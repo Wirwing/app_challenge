e31dd866b2350e9ece8898313f4a44f67d7b96f3
xiumeteo <skylinecesar93@gmail.com>
2014-03-29 08:33:12 -0600

    AÑADIENDO materias

--- appchallenge/conf/evolutions/default/2.sql
+++ appchallenge/conf/evolutions/default/2.sql
@@ -4,7 +4,23 @@
 
 insert into plan values(1, 'matematicas' );
 insert into alumno values(1, '10216287', '' );
-insert into asignatura values(1, 'Calculo 1', 23 );
+
+
+insert into asignatura values(1, 'Algebra Superior 1', 10 );
+insert into asignatura values(2, 'CALCULO 1', 23 );
+insert into asignatura values(3, 'COMPUTACION 1', 10 );
+insert into asignatura values(4, 'ALGEBRA SUPERIOR 2', 10 );
+insert into asignatura values(5, 'CALCULO 2', 23 );
+insert into asignatura values(6, 'COMPUTACION 2', 10 );
+insert into asignatura values(7, 'ALGEBRA LINEAL 1', 10 );
+insert into asignatura values(8, 'ANALISIS NUMERICO 1', 10 );
+insert into asignatura values(9, 'ECUACIONES DIFERENCIALES 1', 10 );
+insert into asignatura values(10, 'ALGEBRA LINEAL 2', 10 );
+insert into asignatura values(11, 'ANALISIS NUMERICO 2', 10 );
+insert into asignatura values(12, 'ECUACIONES DIFERENCIALES 2', 10 );	
+
+
+
 
 
 
