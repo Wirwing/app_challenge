a007ae7e8df5958d2f87720858ca02aa4969d60a
Wirwing <irving.cf@gmail.com>
2014-03-29 04:46:33 -0600

    Testing ws

--- appchallenge/app/controllers/Application.scala
+++ appchallenge/app/controllers/Application.scala
@@ -2,11 +2,31 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.Logger
 
 object Application extends Controller {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def delete = Action { implicit request =>
+    Logger.info("Push delete!")
+    Ok("{status: \"Ok\"}")
+  }
+
+  def login = Action { implicit request =>
+
+      request.headers.get("Authorization") match {
+          case Some(header) => println(header)
+          case None => {
+              println("send user name and password")
+              Unauthorized.withHeaders("WWW-Authenticate" -> "Basic realm=\"myrealm\"")
+          }           
+      }
+
+      Ok("{status: \"Ok auth\"}")
+
   }
 
 }
