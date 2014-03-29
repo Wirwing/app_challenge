3f255a6f54284094188e5369c5f90b638edff080
Wirwing <irving.cf@gmail.com>
2014-03-29 15:26:36 -0600

    Se añade boostrap, js, css y demás eye candy chulo de bonito.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,16 +1,28 @@
 package controllers
 
-import play.api._
 import models._
+import views._
+import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import play.api.data.validation.Constraints._
 import play.api.Logger
 import com.github.nscala_time.time.Imports._
 import play.api.libs.json.Json
 
+import java.util.Date;
 
 class UserNotFoundException(msg: String) extends RuntimeException(msg)
 
+case class SuggestForm(initialHour: String, finalHour: String)
+
 object Application extends Controller {
+
+  val suggestForm = Form(
+    mapping(
+      "initialHour" -> text,
+      "finalHour" -> text)(SuggestForm.apply)(SuggestForm.unapply))
 
   def decodeBasicAuth(auth: String) = {
     val baStr = auth.replaceFirst("Basic ", "")
@@ -19,8 +31,40 @@ object Application extends Controller {
   }
 
   def index = Action { implicit request =>
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.suggestEntry(suggestForm))
   }
+
+  /**
+  * Handle the 'new event form' submission.
+  */
+  def next(): Action[AnyContent] = Action { implicit request =>
+
+    try{
+
+      suggestForm.bindFromRequest.fold(
+        formWithErrors => {
+          BadRequest(html.suggestEntry(formWithErrors))
+          },
+          suggestForm => {
+
+            val formatter = DateTimeFormat.forPattern("HH:mmaa");
+            val start = new LocalTime(formatter.parseDateTime(suggestForm.initialHour))
+            val end = new LocalTime(formatter.parseDateTime(suggestForm.finalHour))
+
+            Logger.info(start.toString)
+            Logger.info(end.toString)
+
+            Redirect(routes.Application.index)
+            })
+
+      } catch {
+
+        case e: Exception =>
+        Redirect(routes.Application.index).flashing("error" -> "Error en los datos del formulario!")
+
+      }
+
+    }
 
   // def index = Action {
 
@@ -48,26 +92,26 @@ object Application extends Controller {
 
       request.headers.get("Authorization").map{ basicAuth =>
 
-          val (name, pass) = decodeBasicAuth(basicAuth)
+        val (name, pass) = decodeBasicAuth(basicAuth)
 
-          Logger.debug(s"Searching for $name with $pass")
+        Logger.debug(s"Searching for $name with $pass")
 
-          Alumno.findByNameAndPassword(name, pass).map{
+        Alumno.findByNameAndPassword(name, pass).map{
           user => 
           Ok(Json.obj("message" -> s"Hello $user identified by $pass"))
           }getOrElse(Ok(Json.obj("message" -> "No lo encontramos! :(")))
 
-      }.getOrElse{
-           NotFound(Json.obj("error" -> "user not found"))
-      }
+            }.getOrElse{
+             NotFound(Json.obj("error" -> "user not found"))
+           }
 
-      }catch{
-          case e: Exception => 
-          {
-            Logger.debug(e.getMessage)
-            NotFound(Json.obj("error" -> "user not found"))
+           }catch{
+            case e: Exception => 
+            {
+              Logger.debug(e.getMessage)
+              NotFound(Json.obj("error" -> "user not found"))
+            }
           }
         }
-      }
 
-    }
+      }
